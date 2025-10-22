65c383f3b3f0ab0beac25f65d7eccac364a4a749
Bruce Alves <brucealves800@gmail.com>
2025-10-22 12:34:12 -0300

    Começando o estudo

---
+++ tecboard/src/App.jsx
@@ -0,0 +1,32 @@
+import './App.css'
+
+function FormularioDeEvento() {
+
+  return (
+    <form className='form-evento'>
+      <h2>Preencha para criar um evento:</h2>
+      <fieldset>
+        <label htmlFor="nome">
+          Qaul o nome do evento?
+        </label>
+        <input type="text" id="nome" placeholder='Summer dev hits' />
+      </fieldset>
+    </form>
+  )
+}
+
+function App() {
+  return (
+    <main>
+      <header>
+        <img src="/logo.pgn" alt="" />
+      </header>
+      <section>
+        <img src="/banner.png" alt="" />
+      </section>
+      <FormularioDeEvento />
+    </main>
+  )
+}
+
+export default App
